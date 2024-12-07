61c871e3d12225b3074438efc49e90cf43e412ca
banskotanup <baskota.anup11@gmail.com>
2024-12-07 15:27:35 +0545

    Add createContainer function

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-const container = document.querySelector("#container");
+document.addEventListener("DOMContentLoaded", function () {
+    createContainer(16);
+    console.log("hi");
+})
 
-const div = document.createElement("div");
-container.appendChild(div);
+function createContainer(size) {
+    let container = document.querySelector("#container");
+    container.style.gridTemplateColumns = `repeat(${size}, 1fe)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fe)`;
+
+    let numDivs = size * size;
+
+    for (let i = 0; i < numDivs; i++){
+        let div = document.createElement("div");
+        div.style.backgroundColor = "yellow";
+        container.insertAdjacentElement("beforeend", div);
+    }
+}
